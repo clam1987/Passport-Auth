a5f64a3400e981e1fa7989dfd166b0778e37e29f
Cuong Lam <lin-1987@live.com>
2019-05-31 15:48:37 -0700

    login and register controller functions

--- server/models/User.js
+++ server/models/User.js
@@ -3,12 +3,7 @@ const Schema = mongoose.Schema;
 
 //Create User Schema
 const UserSchema = new Schema ({
-    firstName: {
-        type: String,
-        required: true
-    },
-
-    lastName: {
+    name: {
         type: String,
         required: true
     },
