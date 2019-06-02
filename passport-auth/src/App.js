a76fcadd80ec67df27e6cfd353e5614c1951277f
Cuong Lam <lin-1987@live.com>
2019-06-02 13:28:43 -0700

    Redux Framework

--- passport-auth/src/App.js
+++ passport-auth/src/App.js
@@ -1,20 +1,29 @@
 // import React from 'react';
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import NavBar from "./components/layouts/NavBar/NavBar";
 import Landing from "./components/layouts/Landing/Landing";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
-
         <NavBar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
