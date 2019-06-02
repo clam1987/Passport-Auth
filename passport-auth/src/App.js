0284933eaa5722e1fcf992510535bc5a49cb8a4f
Cuong Lam <lin-1987@live.com>
2019-06-02 12:41:42 -0700

    Updated Register Page

--- passport-auth/src/App.js
+++ passport-auth/src/App.js
@@ -3,14 +3,18 @@ import React, { Component } from "react";
 import './App.css';
 import NavBar from "./components/layouts/NavBar/NavBar";
 import Landing from "./components/layouts/Landing/Landing";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
+
         <NavBar />
         <Landing />
       </div>
+      </Router>
     );
   }
 }
