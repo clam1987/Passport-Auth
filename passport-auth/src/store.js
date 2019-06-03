66f74ecc074126fc88f6f86f2280e46ccf59531a
Cuong Lam <lin-1987@live.com>
2019-06-02 18:09:48 -0700

    Redux finalization framework

--- passport-auth/src/store.js
+++ passport-auth/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const intialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-    () => [],
+    rootReducer,
     intialState,
     compose(
         applyMiddleware(...middleware),
